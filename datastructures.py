62d5fb1b803da337fb9c5ae3dd94b84547b019df
Mahesh Arya <mahesharya@prmanicus.com>
2026-05-08 18:25:17 +0530

    ccc

--- datastructures.py
+++ datastructures.py
@@ -42,3 +42,51 @@ print(l) # [10, 2, 3, 4, 5]
 l[3:5] = [40, 50]
 print(l) # [10, 2, 3, 40, 50]
 
+# unpacking
+a,b,c=[1,2,3]
+print(a) # 1
+print(b) # 2    
+print(c) # 3
+
+# list methods
+l=[1, 2, 3, 4, 5]
+# append() - adds an element to the end of the list
+# pop() - removes and returns the last element of the list
+# insert() - inserts an element at a specific index
+# remove() - removes the first occurrence of an element
+# sort() - sorts the list in ascending order
+# reverse() - reverses the order of the list
+# count() - returns the number of times an element appears in the list
+# index() - returns the index of the first occurrence of an element
+# clear() - removes all elements from the list
+l.append(6)
+print(l) # [1, 2, 3, 4, 5, 6]
+
+a=l.pop()
+print(l) # [1, 2, 3, 4, 5]
+print(a) # 6
+
+l.insert(3, 10)
+print(l) # [1, 2, 3, 10, 4, 5]
+
+l.remove(10)
+print(l) # [1, 2, 3, 4, 5]
+
+print()
+l.sort()
+print(l) # [1, 2, 3, 4, 5]
+
+l.sort(reverse=True)
+print(l) # [5, 4, 3, 2, 1]
+
+l.reverse()
+print(l) # [1, 2, 3, 4, 5]
+
+l.append(5) # [1, 2, 3, 4, 5, 5]
+
+print(l.count(5)) # 2
+
+print(l.index(5)) # 4
+
+l.clear()
+print(l) # []
