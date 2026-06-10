f7b08200f19f5494f11b64142bcd45c204ddad34
Mahesh Arya <mahesharya@prmanicus.com>
2026-06-10 19:23:35 +0530

    c

--- hello4.py
+++ hello4.py
@@ -49,3 +49,100 @@ temp
 x=10
 x*=5 # x=x+5 
 print(x)
+
+# there are 4 types of basic data types in python
+# int,float,bool,str
+a=10 # int -23423,21341244243
+b=1.1 # float -1.3
+c=True # bool False
+d="hello" # str "10","python",'hi'
+
+# type() - it is an inbuilt function which is used to find the type of a variable or value
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(10))
+print(type(1.1))
+print(type(True))
+print(type("hello"))
+
+# Type conversion
+# int(),float(),str(),bool()
+print(int(1.1)) 
+print(int("10"))
+print(int(10))
+print(int(True))
+print(int(False))
+
+print(float(10))
+print(float("10"))
+print(float(1.1))
+print(float(True))
+print(float(False))
+
+print(str(10))
+print(str(1.1))
+print(str(True))
+print(str(False))
+# str(10)
+# str(1.1)
+# str(True)
+# str(False)
+
+print(bool(10))
+print(bool(1.1))
+print(bool("hello"))
+print(bool(""))
+
+
+# comparison operators
+"""
+>	 	 	 	 greater than
+<          less than
+>=  	 	 	   greater than or equal to
+<=         less than or equal to
+==          equal to
+!=          not equal to
+"""
+print()
+print(10>5.1) # True
+print(10<5) # False
+print(10>=5) # True
+print(10<=5) # False
+print(10==5) # False
+print(10!=5) # True
+
+a=10>5
+b=10<5
+print(type(a),a)
+print(type(b),b)
+
+# logical operators
+# and,or,not
+print(True and True) # True
+print(True and False) # False
+print(False and True) # False
+print(False and False) # False
+
+print(True or True) # True
+print(True or False) # True
+print(False or True) # True
+print(False or False) # False
+
+print(not True) # False
+print(not False) # True
+
+# even odd ex
+# 3%2 -> 1
+# 4%2 -> 0
+# 5%2 -> 1
+# 6%2 -> 0
+# 7%2 -> 1
+# 8%2 -> 0
+
+# multiple of 3 and 5
+# 3%3 -> 0
+# 5%5 -> 0
+# 15%3 -> 0
+# 15%5 -> 0
--- loaneligibility.py
+++ loaneligibility.py
@@ -1,11 +1,13 @@
-# # age should be 18 or above and income greater than 20000 
-# age=int(input("Enter your age: ")) # "12"
-# income=int(input("Enter your income: ")) # "20000"
-
-# print(age>=18 and income>20000)
-
-
 # print True if a is greater than b and False otherwise
-a=int(input("Enter a: "))
-b=int(input("Enter b: "))
-print(a>b)
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# print(a>b)
+
+
+# age should be 18 or above and income greater than 20000 
+age=int(input("Enter your age: ")) # "12"
+income=int(input("Enter your income: ")) # "20000"
+
+print(age>=18 and income>20000)
+# 20 >= 18 and 22000 > 20000
+# True and True -> True
